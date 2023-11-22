760f120ae9ffca8401f4e5fa3100aa0e2407f31c
Ilham WICAKSONO <w4rlock999@yahoo.com>
2023-11-22 23:23:51 +0700

    Add chat page
    Add chat page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,14 @@ import { Console } from 'console';
 export default function Home() {
 
   const [youtubeLink, setYoutubeLink] = useState('')
+  const [chatMessage, setChatMessage] = useState('')
   const [promptSearch, setPromptSearch] = useState('')
+  const [landingMode, setLandingMode] = useState(true)
+  const [chatsHistory, setChatsHistory] = useState([])
 
   const runButtonOnClickHandler = async () => {    
+    setLandingMode(false)
+
     let response = await fetch('/api/initializeWithLink', {
       method: 'POST',
       headers: {
@@ -30,37 +35,61 @@ export default function Home() {
     setYoutubeLink(e.target.value)
   }
 
-  const searchButtonOnClickHandler = async () => {    
-    let response = await fetch('/api/searchWithPrompt', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',        
-      },
-      body: JSON.stringify({promptSearch})
-    })   
-        
+  const chatButtonOnClickHandler = async () => {    
+    // let response = await fetch('/api/searchWithPrompt', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',        
+    //   },
+    //   body: JSON.stringify({promptSearch})
+    // })   
+    setLandingMode(true)        
   }
 
-  const promptOnChangeHandler = (e) => {
-    setPromptSearch(e.target.value)
+  const chatOnChangeHandler = (e) => {
+    setChatMessage(e.target.value)
   }
 
   return (
     <div className = {styles.container}>
-      <div className = {styles.linkContainer}>
-        <input className = {styles.inputLink} type='text' placeholder='Your youtube link here' value={youtubeLink} onChange={linkOnChangeHandler}>
-        </input>
-        <button className = {styles.runButton} onClick={runButtonOnClickHandler}>
-          Start Chat
-        </button>      
+
+      {landingMode == true && 
+        <div className = {styles.linkContainer}>
+          <input className = {styles.linkInput} type='text' placeholder='Paste your youtube link here' value={youtubeLink} onChange={linkOnChangeHandler}>
+          </input>
+          <button className = {styles.runButton} onClick={runButtonOnClickHandler}>
+            Start Chat
+          </button>      
+        </div>
+      }
+
+      {landingMode == false &&              
+        <div className = {styles.chatPageContainer}>
+          <div className = {styles.chatInputAndButtonContainer}>          
+            <input className = {styles.chatInput} type='text' placeholder='Ask anything here!' value={chatMessage} onChange={chatOnChangeHandler}>
+            </input>                    
+            <button className = {styles.sendButton} onClick={chatButtonOnClickHandler}>       
+            ▲       
+            </button>        
+            {/* <button className = {styles.sendButton}>            
+            </button>  */}
+          </div>        
+        </div>     
+      } 
+
+
+
+      <div className = {styles.circle1}>        
       </div>
-      <div className = {styles.promptContainer}>
+      <div className = {styles.circle2}>        
+      </div>
+      {/* <div className = {styles.promptContainer}>
         <input className = {styles.inputPrompt} type='text' placeholder='Prompt search here' value={promptSearch} onChange={promptOnChangeHandler}>
         </input>
         <button className = {styles.searchButton} onClick={searchButtonOnClickHandler}>
           Search
         </button>      
-      </div>
+      </div> */}
     </div>
   )
 }
